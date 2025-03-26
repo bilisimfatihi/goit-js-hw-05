29c28c097c40df5cd1f33d947a05e89df3aec909
bilisimfatihi <bilisimfatihi@gmail.com>
2025-03-26 23:14:00 +0300

    edit task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,3 +1,4 @@
+
 const getTotalBalanceByGender = (users, gender) => {
   return users.reduce((total, user) => {
     return user.gender === gender ? total + user.balance : total;
@@ -5,7 +6,7 @@ const getTotalBalanceByGender = (users, gender) => {
 };
 
 
-const allUsers = [
+const allUsers2 = [
 	{
     name: "Moore Hensley",
     gender: "male",
@@ -43,6 +44,6 @@ const allUsers = [
   }
 ];
 
-console.log(getTotalBalanceByGender(allUsers, "male")); // 12053
+console.log(getTotalBalanceByGender(allUsers2, "male")); // 12053
 
-console.log(getTotalBalanceByGender(allUsers, "female")); // 8863
+console.log(getTotalBalanceByGender(allUsers2, "female")); // 8863
